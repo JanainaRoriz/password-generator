d8005178c79bd3fd15781eedbbd85d3fef8634b0
JanainaRoriz <janainalimar@gmail.com>
2022-07-09 20:53:01 -0300

    Incluí um comando print e iniciei um for loop.

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,5 +1,16 @@
 import random
 
 print('Bem-vindo(a) ao seu gerador de senhas aleatórias')
-caracteres = 'abcdefghijklmnopqrstuvwsyzABCDEFGHIJKLMNOPQRSTUVWSYZ!@#$%&*(),.0123456789'
-number = input('Quantas senhas você quer gerar? ')
+caracteres = 'abcdefghijklmnopqrstuvwsyzABCDEFGHIJKLMNOPQRSTUVWSYZ!@#$%&*^(),.0123456789'
+num_senhas = input('Quantas senhas você quer gerar? ')
+num_senhas = int(num_senhas)
+
+tamanho = input('Quantidade de caracteres da sua senha: ')
+tamanho = int(tamanho)
+
+print('\nEssas são as suas senhas: ')
+
+for senha in range(num_senhas):
+    senhas = ''
+
+
