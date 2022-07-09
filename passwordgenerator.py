e801c484c23f89c81bc8526a8cd30e6018c217a6
JanainaRoriz <janainalimar@gmail.com>
2022-07-08 23:17:55 -0300

    Meu primeiro gerador de senhas aleatórias

---
+++ passwordgenerator.py
@@ -0,0 +1,5 @@
+import random
+
+print('Bem-vindo(a) ao seu gerador de senhas aleatórias')
+caracteres = 'abcdefghijklmnopqrstuvwsyzABCDEFGHIJKLMNOPQRSTUVWSYZ!@#$%&*(),.0123456789'
+number = input('Quantas senhas você quer gerar? ')
